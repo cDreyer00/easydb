9fae4f0d246ab5311b59980ccd0856fdaf170b43
Cristian Dreyer <cristianbr.dreyer@gmail.com>
2023-04-06 08:44:00 -0300

    to update

--- build/src/index.js
+++ build/src/index.js
@@ -4,17 +4,4 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const database_1 = __importDefault(require("./database/database"));
-// const db = new Database('test', ['users', 'posts']);
-// const db = new Database('test');
-// const db = new Database('test', ['guilds']);
-const db = new database_1.default('test', ['guilds', 'users', 'posts']);
-// console.log(db.tables);
-db.deleteTable('users')
-    .then(() => {
-    console.log(db.tables);
-    db.deleteTable('guilds')
-        .then(() => {
-        console.log(db.tables);
-        db.deleteTable('posts').then(() => console.log(db.tables)).catch(err => console.log(err));
-    }).catch(err => console.log(err));
-});
+module.exports = database_1.default;
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,3 @@
 import Database from "./database/database";
 
-// const db = new Database('test', ['users', 'posts']);
-// const db = new Database('test');
-// const db = new Database('test', ['guilds']);
-const db = new Database('test', ['guilds', 'users', 'posts']);
-
-// console.log(db.tables);
-db.deleteTable('users')
-    .then(() => {
-        console.log(db.tables);
-        db.deleteTable('guilds')
-            .then(() => {
-                console.log(db.tables);
-                db.deleteTable('posts').then(() => console.log(db.tables)).catch(err => console.log(err));
-            }).catch(err => console.log(err));
-    })
+module.exports = Database;
