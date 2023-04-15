09adf72e7e6e13f8dbe410f27635b90dcc94c950
CDreyer <54314405+cDreyer00@users.noreply.github.com>
2023-04-14 23:40:23 -0300

    Delete build directory

--- build/src/file-managment/fileManagment.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// import fs from 'fs';
-// import path from 'path';
-// // import { item, getConfig, createOrEditConfig, databasesEntryConfig, databaseConfig } from '../config/config';
-// export function createItem(content: item, path: string) {
-//     const str = JSON.stringify(content);
-//     fs.writeFileSync(path, str);
-// }
